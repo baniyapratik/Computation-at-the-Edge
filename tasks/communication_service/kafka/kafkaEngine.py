27cf69feaf3d1adf83b76f03cf4a2048ac00a846
rgarb16 <rgarb12@gmail.com>
2019-09-08 16:58:04 -0700

    Update kafkaEngine.py

--- tasks/communication_service/kafka/kafkaEngine.py
+++ tasks/communication_service/kafka/kafkaEngine.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('/home/kafka2') 
+sys.path.append('../../tasks') 
 
 from communication_service.communication import Communication
 #from communication import Communication
