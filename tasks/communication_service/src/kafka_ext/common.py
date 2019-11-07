6277864c622328a2f9cf7e1b93c5ae557131a9d5
Clif <cliftonf87@gmail.com>
2019-11-07 01:08:44 -0500

    Add docker-compose file for local communication service deployments

--- tasks/communication_service/src/kafka_ext/common.py
+++ tasks/communication_service/src/kafka_ext/common.py
@@ -1,23 +1,31 @@
 from kafka import KafkaProducer, KafkaConsumer
 import json
-import jsonify 
+import jsonify
+import os
 import time
 from communication_service.utils.logger import LogFactory, ModWatchedFileHandler, SIMPLE_FORMATTER, DEFAULT_LEVEL
 
 
 def get_kafka_producer():
+    print(os.environ.get('KAFKA_SERVER'))
     return KafkaProducer(
-        bootstrap_servers=['10.46.0.1:9092',
-                           '10.38.0.1:9092',
-                           '10.40.0.1:9092'],
+        #bootstrap_servers=['10.46.0.1:9092',
+        #                   '10.38.0.1:9092',
+        #                   '10.40.0.1:9092'],
+        bootstrap_servers=[str(os.environ.get('KAFKA_SERVER1')),
+                           str(os.environ.get('KAFKA_SERVER2')),
+                           str(os.environ.get('KAFKA_SERVER3'))],
         value_serializer=lambda v: json.dumps(v).encode('utf-8'),
         request_timeout_ms=20000)
 
 def get_kafka_consumer():
     return KafkaConsumer(
-        bootstrap_servers=['10.46.0.1:9092',
-                           '10.38.0.1:9092',
-                           '10.40.0.1:9092'],
+        #bootstrap_servers=['10.46.0.1:9092',
+        #                   '10.38.0.1:9092',
+        #                   '10.40.0.1:9092'],
+        bootstrap_servers=[str(os.environ.get('KAFKA_SERVER1')),
+                           str(os.environ.get('KAFKA_SERVER2')),
+                           str(os.environ.get('KAFKA_SERVER3'))],
         auto_offset_reset='earliest',
         consumer_timeout_ms=1000)
 #        value_serializer=lambda v: json.loads(v).encode('utf-8'))
